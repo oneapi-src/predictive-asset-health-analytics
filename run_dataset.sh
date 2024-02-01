d0ea8cd6983ce198ad16a7bdca66ecfe3c0e51ed
Alberto <109159606+aagalleg@users.noreply.github.com>
2024-02-01 13:38:00 -0600

    Update license year to 2024 (#12)

--- run_dataset.sh
+++ run_dataset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2023 Intel Corporation
+# Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 
 [[ -z "$WORKSPACE" ]] && { echo "Error: \$WORKSPACE not declared"; exit 1; }
